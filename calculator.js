a75ada68765696a255cda26d7f15d417e24f32f8
harshit-dugar <dugar87711@gmail.com>
2022-08-01 21:39:53 +0530

    "added post request"

--- calculator.js
+++ calculator.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // parse application/x-www-form-urlencoded
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.sendFile(__dirname+'/index.html');
 });
+app.post('/',(req,res)=>{
+  res.send("Thanks")
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
