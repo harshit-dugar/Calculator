ba35cc6d3889c66ee5036fe8e445f4a6c185de60
harshit-dugar <dugar87711@gmail.com>
2022-09-11 18:31:32 +0530

    modified

--- calculator.js
+++ calculator.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true //extended: true allows for nested objects
 })); // parse application/x-www-form-urlencoded
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 app.post('/',(req,res)=>{
-  res.send("Thanks")
+  let num1 = Number(req.body.n1); //req.body is a object to store the num1 value
+  let num2 = Number(req.body.n2); //in server
+
+  let result = num1 + num2;
+  res.send("Result is : "+result);
 });
 
 app.listen(3000, () => {
